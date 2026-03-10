466d2e915ce6bf0ea3bbc63a99fa160949fcaff4
naingkokolin <naingkokolin007@gmail.com>
2026-03-10 13:16:03 +0630

    added 7 models: achievements, events, faculty, faq, lecturers, partners, timelines

---
+++ backend/models/events.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      default: "Event Title for Default",
+    },
+    type: {
+      type: String,
+      uppercase: true,
+      default: "TECH",
+    },
+    date: {
+      type: String,
+      default: Date.now,
+    },
+    time: {
+      type: String,
+      default: "9:00 am",
+    },
+    venue: {
+      type: String,
+    },
+    description: {
+      type: String,
+      default: "Description",
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["upcoming", "past"],
+      },
+      lowercase: true,
+      default: "upcoming",
+    },
+    speakers: {
+      type: Number,
+      default: 1,
+    },
+    participants: {
+      type: Number,
+      default: 10,
+    },
+    registered: {
+      type: Boolean,
+      default: false,
+    },
+    imageURL: String,
+    videoURL: String,
+  },
+  { timestamps: true },
+);
+
+const Event = mongoose.model("Event", eventSchema)
+export default Event;
