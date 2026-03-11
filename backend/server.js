1019b38598ae8a46b6d2cfd429b43da57bb6a2ea
naingkokolin <naingkokolin007@gmail.com>
2026-03-11 17:18:11 +0630

    testing faqs model

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import publicRoutes from "./routes/publicRoutes.js";
 
 import achievementRoutes from "./routes/achievementRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import facultyRoutes from "./routes/facultyRoutes.js";
 import faqRoutes from "./routes/faqRoutes.js";
-import lecturerRoutes from "./routes/lecturerRoutes.js"
-import timelineRoutes from "./routes/timelineRoutes.js"
-import partnerRoutes from "./routes/partnerRoutes.js"
-
+import lecturerRoutes from "./routes/lecturerRoutes.js";
+import timelineRoutes from "./routes/timelineRoutes.js";
+import partnerRoutes from "./routes/partnerRoutes.js";
 
 const app = express();
 
@@ -19,8 +19,17 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 dotenv.config();
+const mongodbUrl = process.env.MONGODB_URL;
 
 const port = process.env.PORT;
+
+mongoose
+  .connect(mongodbUrl)
+  .then(() => console.log("Connected to Database."))
+  .then(()=> {app.listen(port, () => {
+    console.log(`App is running on http://localhost:${port}`);
+  });})
+  .catch((err) => console.error("MongoDB Error!"));
 
 app.use("/uploads", express.static("uploads"));
 
@@ -28,12 +37,12 @@ app.use("/api/achievements", achievementRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/faculties", facultyRoutes);
 app.use("/api/faqs", faqRoutes);
-app.use("/api/partners", partnerRoutes)
+app.use("/api/partners", partnerRoutes);
 app.use("/api/lecturers", lecturerRoutes);
 app.use("/api/timeline", timelineRoutes);
 
 app.use("/api/", publicRoutes);
 
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App is running on http://localhost:${port}`);
+// });
